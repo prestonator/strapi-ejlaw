a3aec807c42292a1fabf631d7bc649df204469a6
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-11-28 17:40:08 -0600

    Remove strapi-cloud plugin from app.js and package.json

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -3,7 +3,6 @@
  * Any modifications made will be discarded.
  */
 import strapiReactIcons from "@prestonator4000/strapi-react-icons/strapi-admin";
-import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
 import graphql from "@strapi/plugin-graphql/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
@@ -13,7 +12,6 @@ import { renderAdmin } from "@strapi/strapi/admin";
 renderAdmin(document.getElementById("strapi"), {
   plugins: {
     "strapi-react-icons": strapiReactIcons,
-    "strapi-cloud": strapiCloud,
     graphql: graphql,
     i18n: i18N,
     "users-permissions": usersPermissions,
