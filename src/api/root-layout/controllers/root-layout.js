153a818eb2e185ca728001cb92e0d41fad16a426
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-07 15:47:04 -0600

    Add root-layout API files

---
+++ src/api/root-layout/controllers/root-layout.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * root-layout controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::root-layout.root-layout');
