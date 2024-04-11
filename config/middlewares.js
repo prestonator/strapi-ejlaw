87c931b92fd02ef7a2b88b9864b8581dbb82c3ac
Preston Jenkins <preston@eltonjenkinslaw.com>
2024-04-11 14:29:04 -0500

    Update middlewares and plugins configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,37 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+            `${env("DO_SPACE_CDN")}`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+            `${env("DO_SPACE_CDN")}`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,17 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
+        cdn: env("DO_SPACE_CDN"),
+      },
+    },
+  },
   graphql: {
     enabled: true,
     config: {
@@ -31,4 +44,4 @@ module.exports = {
       },
     },
   },
-};
+});
